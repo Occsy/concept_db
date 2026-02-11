961c3e37788018392fed1ae86ca36f0074c612df
Occsy <thulunitasonriente15@gmail.com>
2026-02-11 19:02:40 -0400

    error handling update
    partially updated error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,45 @@
 pub mod elaborate {
-    use std::collections::HashMap;
-    use std::fmt::Debug;
-    use std::fs::{self, File};
-    use std::io::{BufReader, Read, Write};
-    use std::path::Path;
     use serde::{Serialize, de::DeserializeOwned};
     use serde_json::to_string;
+    use std::collections::HashMap;
+    use std::fmt::Debug;
+    use std::fs::{self, DirEntry, File};
+    use std::io::{BufReader, Read, Write};
+    use std::path::Path;
 
     #[derive(Debug)]
     pub enum TErrors {
-        FileNotFound, 
-        FileError, 
-        ReadByteError, 
-        StringConvert
+        FileNotFound,
+        FileError,
+        DirError,
+        ReadByteError,
+        WriteByteError,
+        StringConvert,
+        HashConvert,
+        DeleteError,
     }
 
-
-    /// covers the different kinds of HashMap that may be required for conversion 
-    /// from intended struct. 
+    /// covers the different kinds of HashMap that may be required for conversion
+    /// from intended struct.
     pub trait ToHash {
-        fn to_hash(&self) -> Result<HashMap<String, String>, serde_json::Error>; 
-        fn to_hash_opt(&self) -> serde_json::Result<HashMap<String, Option<String>>>;
-        fn to_hash_vec(&self) -> Result<HashMap<String, Vec<String>>, serde_json::Error>;  
+        fn to_hash(&self) -> Result<HashMap<String, String>, TErrors>;
+        fn to_hash_opt(&self) -> Result<HashMap<String, Option<String>>, TErrors>;
+        fn to_hash_vec(&self) -> Result<HashMap<String, Vec<String>>, TErrors>;
     }
 
     trait ToHashOpt {
-        fn convert_opt(&self) -> HashMap<String, Option<String>>; 
+        fn convert_opt(&self) -> HashMap<String, Option<String>>;
     }
 
     impl ToHashOpt for HashMap<String, String> {
         fn convert_opt(&self) -> HashMap<String, Option<String>> {
-            let mut temp_hash: HashMap<String, Option<String>> =   HashMap::new();  
-            
-            self.into_iter().for_each(|(k, v)| {
-                let v: Option<String> = if v.len()>0 {
-                    Some(v.clone()) 
-                } else {
-                    None
-                }; 
+            let mut temp_hash: HashMap<String, Option<String>> = HashMap::new();
 
-                temp_hash.insert(k.clone(), v); 
-            }); 
+            self.into_iter().for_each(|(k, v)| {
+                let v: Option<String> = if v.len() > 0 { Some(v.clone()) } else { None };
+
+                temp_hash.insert(k.clone(), v);
+            });
 
             temp_hash
         }
@@ -49,33 +48,41 @@ pub mod elaborate {
     /// added functionality to count iterations of an item in a vector
     /// of a generic type
     pub fn count_val<T: std::fmt::Debug + Eq>(vec: Vec<T>, value: String) -> usize {
-        vec.into_iter().map(|c| format!("{c:?}").trim().to_string()).filter(|v| *v==value.trim().to_string()).collect::<Vec<String>>().len()
-    } 
+        vec.into_iter()
+            .map(|c| format!("{c:?}").trim().to_string())
+            .filter(|v| *v == value.trim().to_string())
+            .collect::<Vec<String>>()
+            .len()
+    }
 
-    /// converts HashMap<String, String> to T 
-    pub fn read_hash<T: Serialize + DeserializeOwned + Sized + Clone>(hash: HashMap<String, String>)->Result<T, serde_json::Error>{
-                let convert1: String = serde_json::to_string(&hash).unwrap();
+    /// converts HashMap<String, String> to T
+    pub fn read_hash<T: Serialize + DeserializeOwned + Sized + Clone>(
+        hash: HashMap<String, String>,
+    ) -> Result<T, TErrors> {
+        let Ok(convert1) = serde_json::to_string(&hash) else {
+            return Err(TErrors::StringConvert);
+        };
 
-                let convert2: T = serde_json::from_str(&convert1).unwrap();
+        let Ok(convert2) = serde_json::from_str(&convert1) else {
+            return Err(TErrors::StringConvert);
+        };
 
-                Ok(convert2)
+        Ok(convert2)
     }
 
     #[derive(Debug, Serialize)]
-    /// Takes in a struct of type T, 
-    /// then applies concept_db functionality 
+    /// Takes in a struct of type T,
+    /// then applies concept_db functionality
     pub struct Fragment<T: Serialize + DeserializeOwned + Sized + Clone> {
         pub inner: T,
     }
 
     impl<T: Serialize + DeserializeOwned + Sized + Clone + Debug> ToString for Fragment<T> {
-        
-        /// converts T to String 
+        /// converts T to String
         fn to_string(&self) -> String {
-
             let Ok(output) = to_string(&self.inner) else {
-                panic!("unable to convert: {self:?}"); 
-            }; 
+                panic!("unable to convert: {self:?}");
+            };
 
             output
         }
@@ -83,93 +90,115 @@ pub mod elaborate {
 
     impl<T: Serialize + DeserializeOwned + Sized + Clone + Debug> ToHash for Fragment<T> {
         /// converts T to HashMap<String, String>
-        fn to_hash(&self) -> Result<HashMap<String, String>, serde_json::Error> where HashMap<String, String>: Serialize{
-            serde_json::from_str::<HashMap<String, String>>(&self.to_string())
+        fn to_hash(&self) -> Result<HashMap<String, String>, TErrors>
+        where
+            HashMap<String, String>: Serialize,
+        {
+            let Ok(output) = serde_json::from_str::<HashMap<String, String>>(&self.to_string())
+            else {
+                return Err(TErrors::StringConvert);
+            };
+
+            Ok(output)
         }
         /// converts T to HashMap<String, Option<String>>
-        fn to_hash_opt(&self) -> serde_json::Result<HashMap<String, Option<String>>> where HashMap<String, Option<String>>: Serialize{
-            Ok(self.to_hash()?.convert_opt())
+        fn to_hash_opt(&self) -> Result<HashMap<String, Option<String>>, TErrors>
+        where
+            HashMap<String, Option<String>>: Serialize,
+        {
+            let Ok(output) = self.to_hash() else {
+                return Err(TErrors::HashConvert);
+            };
+
+            Ok(output.convert_opt())
         }
 
         /// converts T to HashMap<String, Vec<String>>
-        fn to_hash_vec(&self) -> Result<HashMap<String, Vec<String>>, serde_json::Error> {
-            serde_json::from_str::<HashMap<String, Vec<String>>>(&self.to_string())
+        fn to_hash_vec(&self) -> Result<HashMap<String, Vec<String>>, TErrors> {
+            let Ok(output) =
+                serde_json::from_str::<HashMap<String, Vec<String>>>(&self.to_string())
+            else {
+                return Err(TErrors::StringConvert);
+            };
+
+            Ok(output)
         }
     }
-
-
 
     impl<T: Serialize + DeserializeOwned + Sized + Clone + Debug> Fragment<T> {
         /// initializes the Fragment<T>
         pub fn new(inner: T) -> Self
         where
-            T: Serialize + DeserializeOwned + Sized + Clone
+            T: Serialize + DeserializeOwned + Sized + Clone,
         {
             Self { inner }
         }
 
         /// parses file if exists then returns a new Fragment<T>
-        pub fn read_table(
-            &self,
-            file_path: String,
-        ) -> Result<Fragment<T>, TErrors> 
-        {
+        pub fn read_table(&self, file_path: String) -> Result<Fragment<T>, TErrors> {
             let path: String = format!("./db_files/{}.json", file_path);
 
-            let convert_path: &Path = Path::new(&path);  
+            let convert_path: &Path = Path::new(&path);
 
             if !convert_path.exists() {
-                return Err(TErrors::FileNotFound)
+                return Err(TErrors::FileNotFound);
             }
 
-            let f: File = File::open(convert_path).map_err(|_| TErrors::FileError)?;
+            let Ok(f) = File::open(convert_path) else {
+                return Err(TErrors::FileError);
+            };
 
             let reader: BufReader<File> = BufReader::new(f);
 
-            let inner_value: T = serde_json::from_reader(reader)
-                .map_err(|_| TErrors::ReadByteError)?; 
+            let Ok(inner_value) = serde_json::from_reader(reader) else {
+                return Err(TErrors::ReadByteError);
+            };
 
-            let new_value: Fragment<T> = Self::new(inner_value);  
+            let new_value: Fragment<T> = Self::new(inner_value);
 
             Ok(new_value)
         }
 
-        /// creates a new json file and inputs the table 
-        /// created using the struct 
-        pub fn create_table(&self, table_name: String) -> std::io::Result<&Self> { 
-
-            let path_root: &String  = &String::from("./db_files/"); 
+        /// creates a new json file and inputs the table
+        /// created using the struct
+        pub fn create_table(&self, table_name: String) -> Result<&Self, TErrors> {
+            let path_root: &String = &String::from("./db_files/");
 
             if !Path::new(path_root).is_dir() {
-                std::fs::create_dir(path_root).unwrap()
+                std::fs::create_dir(path_root).map_err(|_| {
+                    return TErrors::DirError;
+                })?;
             }
 
             if Path::new(&format!("{}{}.json", path_root, table_name)).is_file() {
-                return Ok(self); 
+                return Ok(self);
             }
 
-            let string_convert: String = self.to_string(); 
+            let string_convert: String = self.to_string();
 
-            let mut src_bytes = string_convert.as_bytes(); 
+            let mut src_bytes = string_convert.as_bytes();
 
-            let inner_path: &String = &format!("{path_root}{}", table_name + ".json").to_string();  
+            let inner_path: &String = &format!("{path_root}{}", table_name + ".json").to_string();
 
-            let full_path: &Path = &Path::new(inner_path);  
-            
-            let mut file: File = File::create_new(full_path).unwrap();
+            let full_path: &Path = &Path::new(inner_path);
 
-            file.write(&mut src_bytes).unwrap();
-            
+            let Ok(mut file) = File::create_new(full_path) else {
+                return Err(TErrors::FileError);
+            };
+
+            file.write(&mut src_bytes).map_err(|_| {
+                return TErrors::WriteByteError;
+            })?;
+
             Ok(self)
         }
 
-        /// deletes the table in question 
+        /// deletes the table in question
         pub fn delete_table(&self, table_name: String) -> std::io::Result<()> {
+            let path: &String = &format!("./db_files/{}.json", table_name);
 
-            let path: &String = &format!("./db_files/{}.json", table_name); 
-            
-            let convert_path: &Path = Path::new(path); 
-            
+            let convert_path: &Path = Path::new(path);
+
             if convert_path.is_file() {
                 std::fs::remove_file(path)?
             }
@@ -177,15 +206,26 @@ pub mod elaborate {
             Ok(())
         }
         /// deletes table if tables contents match T of Fragment<T>
-        pub fn delete_table_infer(&self) -> std::io::Result<()> where Fragment<T>: DeserializeOwned{
-            let dir = fs::read_dir("./db_files/").unwrap();
+        pub fn delete_table_infer(&self) -> Result<(), TErrors>
+        where
+            Fragment<T>: DeserializeOwned,
+        {
+            let Ok(dir) = fs::read_dir("./db_files/") else {
+                return Err(TErrors::DirError);
+            };
             for entry in dir.into_iter().filter_map(|f| f.ok()) {
                 let mut context = [0; 10];
-                let mut file: std::fs::File = File::open(entry.path()).unwrap();
-                file.read(&mut context).unwrap(); 
+                let Ok(mut file) = File::open(entry.path()) else {
+                    return Err(TErrors::FileError);
+                };
+                file.read(&mut context).map_err(|_| {
+                    return TErrors::ReadByteError;
+                })?;
                 let string_context: String = String::from_utf8_lossy(&context).to_string();
                 if let Ok(_) = serde_json::from_str::<Self>(&string_context) {
-                    fs::remove_file(entry.path()).unwrap();
+                    fs::remove_file(entry.path()).map_err(|_| {
+                        return TErrors::DeleteError;
+                    })?;
                 } else {
                     continue;
                 }
@@ -193,24 +233,37 @@ pub mod elaborate {
             Ok(())
         }
 
-        /// returns all regardless of type of T 
-        pub fn get_all(&self) -> std::io::Result<Vec<HashMap<String, String>>> {
+        /// returns all regardless of type of T
+        pub fn get_all(&self) -> Result<Vec<HashMap<String, String>>, TErrors> {
             let mut temp_vec: Vec<HashMap<String, String>> = Vec::new();
-            for entry in fs::read_dir("./db_files/")?.into_iter().filter_map(|f| f.ok()) {
-                let obj: Result<Fragment<T>, TErrors> = self.read_table(entry.path().to_string_lossy().to_string());
-                temp_vec.push(obj.unwrap().to_hash().unwrap());
+            for entry in fs::read_dir("./db_files/")
+                .map_err(|_| {
+                    return TErrors::DirError;
+                })?
+                .into_iter()
+                .filter_map(|f| f.ok())
+                .collect::<Vec<DirEntry>>()
+            {
+                let Ok(obj) = self.read_table(entry.path().to_string_lossy().to_string()) else {
+                    return Err(TErrors::ReadByteError);
+                };
+
+                let Ok(obj_hash) = obj.to_hash() else {
+                    return Err(TErrors::HashConvert);
+                };
+                temp_vec.push(obj_hash);
             }
             Ok(temp_vec)
         }
 
-        /// returns only matching initial type of T 
+        /// returns only matching initial type of T
         pub fn get_all_infer(&self) -> std::io::Result<Vec<Fragment<T>>> {
             let mut temp_vec: Vec<Fragment<T>> = Vec::new();
 
             for entry in fs::read_dir("./db_files/")?
                 .into_iter()
-                .filter_map(|f| f.ok()) 
-            {   
+                .filter_map(|f| f.ok())
+            {
                 if let Ok(obj) = self.read_table(entry.path().to_string_lossy().to_string()) {
                     temp_vec.push(obj);
                 } else {
@@ -221,45 +274,66 @@ pub mod elaborate {
             Ok(temp_vec)
         }
 
-        /// combines 2 tables 
-        pub fn merge(&self, foreign_table: HashMap<String, String>) -> HashMap<String, String>
-        {
-            let mut hashed_self: HashMap<String, String> = self.to_hash().unwrap();
+        /// combines 2 tables
+        pub fn merge(
+            &self,
+            foreign_table: HashMap<String, String>,
+        ) -> Result<HashMap<String, String>, TErrors> {
+            let Ok(mut hashed_self) = self.to_hash() else {
+                return Err(TErrors::HashConvert);
+            };
             for (key, value) in foreign_table {
                 hashed_self.insert(key, value);
-            } 
-            hashed_self
+            }
+
+            Ok(hashed_self)
         }
 
-        /// performs a left join on 2 tables 
+        /// performs a left join on 2 tables
         pub fn left_join(
             &self,
             foreign_table: HashMap<String, Option<String>>,
-        ) -> Result<HashMap<String, String>, serde_json::Error>
-        {
+        ) -> Result<HashMap<String, String>, serde_json::Error> {
             let mut self_hashed: HashMap<String, String> = self.to_hash().unwrap();
 
-            let key_vals: Vec<(String, String)> = foreign_table.iter().filter(|(k, _)| self_hashed.contains_key(*k)).map(|(k2, v2)| {
-                let v2: &Option<String> = if v2.is_some() {
-                    v2
-                } else {
-                    &Some("None".to_string())
-                }; 
-                return (format!("{}_{}", k2.clone(), count_val(foreign_table.iter().collect(), k2.clone())), v2.clone().unwrap()); 
-            }).collect(); 
+            let key_vals: Vec<(String, String)> = foreign_table
+                .iter()
+                .filter(|(k, _)| self_hashed.contains_key(*k))
+                .map(|(k2, v2)| {
+                    let v2: &Option<String> = if v2.is_some() {
+                        v2
+                    } else {
+                        &Some("None".to_string())
+                    };
+                    return (
+                        format!(
+                            "{}_{}",
+                            k2.clone(),
+                            count_val(foreign_table.iter().collect(), k2.clone())
+                        ),
+                        v2.clone().unwrap(),
+                    );
+                })
+                .collect();
 
             key_vals.into_iter().for_each(|(k, v)| {
-                self_hashed.insert(k, v); 
+                self_hashed.insert(k, v);
             });
 
             Ok(self_hashed)
         }
 
-        /// sorts tables based on key + value 
+        /// sorts tables based on key + value
         pub fn build_where(&self, key: String, value: String) -> Vec<Self> {
             let mut temp_vec: Vec<Fragment<T>> = Vec::new();
-            for entry in fs::read_dir("./db_files/").unwrap().into_iter().filter_map(|f| f.ok()) {
-                let contents: Fragment<T> = self.read_table(entry.path().to_string_lossy().to_string()).unwrap();
+            for entry in fs::read_dir("./db_files/")
+                .unwrap()
+                .into_iter()
+                .filter_map(|f| f.ok())
+            {
+                let contents: Fragment<T> = self
+                    .read_table(entry.path().to_string_lossy().to_string())
+                    .unwrap();
                 let hashed_contents: HashMap<String, String> = contents.to_hash().unwrap();
                 if hashed_contents.contains_key(&key)
                     && hashed_contents.get(&key).unwrap().trim().to_string()
@@ -271,24 +345,25 @@ pub mod elaborate {
             temp_vec
         }
 
-        /// update key in table 
-        pub fn update_table(&self, table_name: String, key: String, value: String) -> T{
-            let current_table: Self = self.read_table( table_name).unwrap(); 
+        /// update key in table
+        pub fn update_table(&self, table_name: String, key: String, value: String) -> T {
+            let current_table: Self = self.read_table(table_name).unwrap();
 
             let mut hashed_table: HashMap<String, String> = current_table.to_hash().unwrap();
 
-            hashed_table.insert(key, value); 
+            hashed_table.insert(key, value);
 
             serde_json::from_str::<T>(&serde_json::to_string(&hashed_table).unwrap()).unwrap()
         }
 
         /// updates table's key and value if value is type of Vec<String>
         pub fn update_table_vec(&self, table_name: String, key: String, value: Vec<String>) -> T {
-            let current_table: Self = self.read_table( table_name).unwrap(); 
+            let current_table: Self = self.read_table(table_name).unwrap();
 
-            let mut hashed_table: HashMap<String, Vec<String>> = current_table.to_hash_vec().unwrap();
+            let mut hashed_table: HashMap<String, Vec<String>> =
+                current_table.to_hash_vec().unwrap();
 
-            hashed_table.insert(key, value); 
+            hashed_table.insert(key, value);
 
             serde_json::from_str::<T>(&serde_json::to_string(&hashed_table).unwrap()).unwrap()
         }
